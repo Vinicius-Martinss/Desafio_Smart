901d82ede572d1c5e70e2a68c130aa6a9cce8e1f
Vinicius-Martinss <viniciusmartinsgsi.com>
2025-05-23 10:47:14 -0300

    register e logo ajeitado

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -6,11 +6,11 @@
 
         <x-validation-errors class="mb-4" />
 
-        @session('status')
+        @if (session('status'))
             <div class="mb-4 font-medium text-sm text-green-600">
-                {{ $value }}
+                {{ session('status') }}
             </div>
-        @endsession
+        @endif
 
         <form method="POST" action="{{ route('login') }}">
             @csrf
@@ -25,23 +25,31 @@
                 <x-input id="password" class="block mt-1 w-full" type="password" name="password" required autocomplete="current-password" />
             </div>
 
-            <div class="block mt-4">
+            <div class="flex items-center justify-between mt-4">
                 <label for="remember_me" class="flex items-center">
                     <x-checkbox id="remember_me" name="remember" />
                     <span class="ms-2 text-sm text-gray-600">{{ __('Remember me') }}</span>
                 </label>
-            </div>
 
-            <div class="flex items-center justify-end mt-4">
                 @if (Route::has('password.request'))
                     <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('password.request') }}">
                         {{ __('Forgot your password?') }}
                     </a>
                 @endif
-
-                <x-button class="ms-4">
+            </div>
+            
+            <div class="flex justify-center mt-6">
+                <x-button class="w-full max-w-xs text-center justify-center">
                     {{ __('Log in') }}
                 </x-button>
+            </div>
+
+            <div class="mt-4 text-center">
+                @if (Route::has('register'))
+                    <a href="{{ route('register') }}" class="underline text-sm text-gray-600 hover:text-gray-900">
+                        {{ __('Não possui cadastro? Criar conta') }}
+                    </a>
+                @endif
             </div>
         </form>
     </x-authentication-card>
--- routes/web.php
+++ routes/web.php
@@ -1,17 +1,14 @@
 <?php
 
+use GuzzleHttp\Psr7\FnStream;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 }) ->name('home');
 
-Route::middleware([
-    'auth:sanctum',
-    config('jetstream.auth_session'),
-    'verified',
-])->group(function () {
-    Route::get('/dashboard', function () {
-        return view('dashboard');
-    })->name('dashboard');
+Route::middleware(['auth:sanctum', config('jetstream.auth_session'),'verified',])
+->group(function () {
+    Route::view('/dashboard', 'dashboard')->name('dashboard');
+
 });
